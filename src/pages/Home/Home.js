0ae0c6fdaede6656679ed0c4c576ef36bf717f39
Jean198 <jeandedieu.niyigaba@gmail.com>
2023-02-13 20:57:01 +0200

    Homepage and auth pages

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Home.css';
+import homeImage from '../../assets/images/home-page.webp';
 
 const Home = () => {
   return (
@@ -10,18 +11,45 @@ const Home = () => {
           <h2>Inventory App</h2>
           <ul>
             <li>
-              <Link to='/' className='navlink'>
+              <Link to='/login' className='navlink'>
+                Dashboard
+              </Link>
+            </li>
+            <li>
+              <Link to='/register' className='navlink'>
                 Register
               </Link>
             </li>
             <li>
-              <Link to='/' className='navlink'>
+              <Link to='/login' className='navlink'>
                 Login
               </Link>
             </li>
           </ul>
         </nav>
       </div>
+      <section className='hero-section container'>
+        <div className='row'>
+          <div className='hero-text col-lg-6'>
+            <h2>
+              Inventory {'&'} <br /> Stock Management Solution
+            </h2>
+
+            <p className='mt-4'>
+              Inventory system to control and manage products in the warehouse
+              in real time and integrated to make it easier to develop your
+              business
+            </p>
+
+            <button className='btn btn-lg btn-outline-light mt-4'>
+              Free Trial 1 Month
+            </button>
+          </div>
+          <div className='hero-image col-lg-6'>
+            <img src={homeImage} alt='' className='home-image' />
+          </div>
+        </div>
+      </section>
     </div>
   );
 };

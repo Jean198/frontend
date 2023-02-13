808818343c098cfcbb47ab87142431918550d4e8
Jean198 <jeandedieu.niyigaba@gmail.com>
2023-02-13 14:46:02 +0200

    create homwpage navigation bar

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Home.css';
 
 const Home = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className='home'>
+      <div className='container'>
+        <nav>
+          <h2>Inventory App</h2>
+          <ul>
+            <li>
+              <Link to='/' className='navlink'>
+                Register
+              </Link>
+            </li>
+            <li>
+              <Link to='/' className='navlink'>
+                Login
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
